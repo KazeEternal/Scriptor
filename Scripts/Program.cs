02064185522a508f2e837ab46c89bccfa6ea86c0
Kyle Peplow <kyle.a.peplow@tutamail.com>
2021-12-04 20:38:09 -0500

    Added Support for Test Output Files project.
    Added Support for Reverse Order Renaming by disc.

--- Scripts/Program.cs
+++ Scripts/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Linq;
 using System.Configuration;
+using System.Reflection;
+using System.Collections.Generic;
 
 namespace Scripts
 {
@@ -9,32 +11,33 @@ namespace Scripts
     {
         static void Main(string[] args)
         {
-            string mShowName = Properties.Script.Default.DefaultShowName;
+            List<MethodInfo> routines = new List<MethodInfo>();
+            string showName = Properties.Script.Default.DefaultShowName;
 
             Console.WriteLine("---TV SERIES TOOL---");
             Console.WriteLine("String in Parenthesis is default value, press enter without entering anything to use.\n");
-            Console.WriteLine("Enter Show Name ({0}): ", mShowName);
-            String? readShow = Console.ReadLine();
+            Console.WriteLine("Enter Show Name ({0}): ", showName);
+            showName = Console.ReadLine();
             int seasonNumber = -1;
 
             Console.Clear();
-            if (string.IsNullOrWhiteSpace(readShow))
+            if (!string.IsNullOrWhiteSpace(showName))
             {
-                Console.WriteLine("Using name: {0}", mShowName);
+                Console.WriteLine("Using name: {0}", showName);
+                Properties.Script.Default.DefaultShowName = showName;
+                Properties.Script.Default.Save();
             }
             else
             {
-                mShowName = readShow;
-                Console.WriteLine("Show name set to '{0}'", mShowName);
-                Properties.Script.Default.DefaultShowName = mShowName;
-                Properties.Script.Default.Save();
+                showName = Properties.Script.Default.DefaultShowName;
+                Console.WriteLine("Show name set to '{0}'", showName);
             }
 
             Console.Clear();
             do
             {
                 Console.WriteLine("Enter Season Number:");
-                if(int.TryParse(Console.ReadLine(), out seasonNumber))
+                if (int.TryParse(Console.ReadLine(), out seasonNumber))
                 {
 
                 }
@@ -46,36 +49,43 @@ namespace Scripts
 
             Console.Clear();
 
-            Console.WriteLine("Input Path: ");
+            Console.WriteLine("Input Path ({0}): ", Properties.Script.Default.InputPath);
             string path = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(path))
+            if (!string.IsNullOrWhiteSpace(path))
             {
-                Console.WriteLine("Using name: {0}", path);
+                Properties.Script.Default.InputPath = path;
+                Properties.Script.Default.Save();
             }
             else
             {
-                mShowName = readShow;
-                Console.WriteLine("Show name set to '{0}'", path);
-                Properties.Script.Default.InputPath = path;
-                Properties.Script.Default.Save();
-
+                path = Properties.Script.Default.InputPath;
             }
             Console.Clear();
 
-            Console.WriteLine("Show Name: {0}", mShowName);
+            Console.WriteLine("Show Name: {0}", showName);
             Console.WriteLine("Season Number: {0}", seasonNumber.ToString("00"));
 
             Console.WriteLine("======Select a Routine======");
 
             int count = 1;
-            foreach(var item  in typeof(Program).GetMethods().Where(method => method.GetCustomAttributes(typeof(ExecutorRoutine), true).Length > 0))
+            foreach (MethodInfo item in typeof(Program).GetMethods().Where(method => method.GetCustomAttributes(typeof(ExecutorRoutine), true).Length > 0))
             {
                 ExecutorRoutine routine = (ExecutorRoutine)item.GetCustomAttributes(typeof(ExecutorRoutine), true)[0];
                 Console.WriteLine("\t({0}) {1}", count++, routine.DisplayName);
+                routines.Add(item);
             }
 
-            RenameShowByOrder(mShowName, seasonNumber, path);
+            Console.Write("Script Number: ");
+            int decision;
+            while (!int.TryParse(Console.ReadLine(), out decision))
+            {
+                Console.Write("Script Number: ");
+            }
+
+            MethodInfo method = routines[decision - 1];
+            method.Invoke(null, new object[] { showName, seasonNumber, path });
+            //RenameShowByOrder(mShowName, seasonNumber, path);
         }
 
         [ExecutorRoutine("Rename Files By Order", "Renames the file based on their alphabetical order to the episode order")]
@@ -98,7 +108,7 @@ namespace Scripts
         }
 
         [ExecutorRoutine("Rename Files By disc in reverse order", "Takes each disc grouping and renames them from the last file to the first for the episode number.")]
-        public static void RenameShowByDiscReverseOrder(string showName, int seasonNumber, string path, string discRegEx)
+        public static void RenameShowByDiscReverseOrder(string showName, int seasonNumber, string path)
         {
             DirectoryInfo dInfo = new DirectoryInfo(path);
 
@@ -124,11 +134,12 @@ namespace Scripts
                     foreach (FileInfo fInfo in discFiles)
                     {
                         string oldName = fInfo.Name;
-                        string outputName = string.Format("{0} - S{1}E{2}{3}.", showName, seasonNumber.ToString("00"), episodeNumber.ToString("00"), fInfo.Extension);
+                        string outputName = string.Format("{0} - S{1}E{2}{3}", showName, seasonNumber.ToString("00"), episodeNumber.ToString("00"), fInfo.Extension);
                         fInfo.MoveTo(Path.Combine(dInfo.FullName, outputName));
                         Console.WriteLine("Episode {0}\n\tOldName: {1}\n\tNewName: {2}", episodeNumber.ToString("00"), oldName, outputName);
                         episodeNumber++;
                     }
+                    discNumber++;
                 }
             }
         }
---
+++ Test Output/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Test_Ouput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string outputPath = @"D:\Encoding\Video\Test";
+            int discNumber = 0;
+            int fileNumber = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    discNumber++;
+                }
+
+                FileInfo fileInfo = new FileInfo(Path.Combine(outputPath, String.Format("Test Show From Make {0} {1}-{2}.txt", "Disc", discNumber, fileNumber++)));
+
+                using (StreamWriter writer = fileInfo.CreateText())
+                {
+                    writer.WriteLine(fileInfo.Name);
+                }
+            }
+        }
+    }
+}
